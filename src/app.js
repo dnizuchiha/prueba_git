027c7ed2a38d4c4bd82007cc21593867229d915c
dnizuchiha <dmauricio.uchiha@gmail.com>
2021-04-14 13:54:09 -0600

    se hizo algo

--- src/app.js
+++ src/app.js
@@ -4,7 +4,3 @@ const app = express();
 //settings
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
-
-app.listen(3000, () =>{
-    console.log('escuchando en el puerto 3000');
-});
